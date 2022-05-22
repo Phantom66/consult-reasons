2c2f2c68a43aee2476a5dbe774c4f4c62ca08619
Andres Leotur <leoturorellana@gmail.com>
2022-05-21 22:18:01 -0400

    mapeando las tablas

--- src/main/java/com/core/consultreasons/model/TypeReason.java
+++ src/main/java/com/core/consultreasons/model/TypeReason.java
@@ -1,19 +1,24 @@
 package com.core.consultreasons.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name="TYPE_REASON")
 public class TypeReason {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
     private Long id;
+    @Column(name = "DESC_REASON")
     private String descReason;
     
 }
